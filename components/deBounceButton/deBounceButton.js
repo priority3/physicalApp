c9980a114ab672e8f984397ab0690b18fc87f5f1
priority <1832052104@qq.com>
2022-03-09 00:22:27 +0800

    feature:login页面

---
+++ components/deBounceButton/deBounceButton.js
@@ -0,0 +1,49 @@
+// components/deBounceButton/deBounceButton.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    type:{
+      type:String,
+      value:"info"
+    },
+    size:{
+      type:String,
+      value:"normal"
+    },
+    disabled:{
+      type:Boolean,
+      value:false
+    },
+    loading:{
+      type:Boolean,
+      value:false
+    },
+    customStyle:{
+      type:String,
+      value:""
+    },
+    btnValue:{
+      type:String,
+      value:"按钮"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    btnClick(){
+      
+      this.triggerEvent('loginClick', { xixi: "666传值成功" });
+    }
+  }
+})
